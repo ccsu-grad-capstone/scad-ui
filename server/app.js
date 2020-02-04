0bdd01f13e33c709c33501ce52b15b4e521a05fb
Ryan Lauzon <lauzon232@gmail.com>
2020-02-03 21:58:43 -0500

    removing unneccessary code

--- server/app.js
+++ server/app.js
@@ -19,24 +19,6 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
-// app.use(session({ secret: 'scadfantasy' }))
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404))
-// })
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message
-//   res.locals.error = req.app.get('env') === 'development' ? err : {}
-
-//   // render the error page
-//   res.status(err.status || 500)
-//   res.render('error')
-// })
-
-// require('./config/passport-setup.js')(app)
-// require('./config/passport-yahoo.js')(app)
 
 app.use(express.static(path.join(__dirname, '/public/')))
 //views to use if we're displaying anything in the brower from this url (localhost:4000)
