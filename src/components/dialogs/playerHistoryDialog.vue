0beef4b81d0bdd96d8e692d75d46f52549c453b4
Ryan Lauzon <lauzon232@gmail.com>
2021-03-29 10:36:53 -0400

    updated playerHistoryDialog loading location

--- src/components/dialogs/playerHistoryDialog.vue
+++ src/components/dialogs/playerHistoryDialog.vue
@@ -1,31 +1,32 @@
 <template lang="pug">
   q-dialog(v-model='visable' @hide="triggerDialog()")
-    loading(v-if="!loaded" :message="'Fetching Player History...'")
-    q-card(v-else style="width: 800px; max-width: 80vw;")
-      .row.items-center.q-pa-sm.bg-grey-4
-        .row.full-width.q-gutter-xs.items-center.justify-center
-          q-space
-          q-avatar(size="70px" color='white')
-            img.q-pa-xs(:src="yahooPlayer.headshot.url" style="width: 85%")
-          .text-h4.q-pl-md.text-weight-bolder {{yahooPlayer.name.full}}
-          q-space
-          q-btn(icon="close" flat round dense @click="triggerDialog()")
+    q-card(style="width: 800px; max-width: 80vw;")
+      loading(v-if="!loaded" :message="'Fetching Player History...'")
+      .row(v-else)
+        .row.items-center.q-pa-sm.bg-grey-4
+          .row.full-width.q-gutter-xs.items-center.justify-center
+            q-space
+            q-avatar(size="70px" color='white')
+              img.q-pa-xs(:src="yahooPlayer.headshot.url" style="width: 85%")
+            .text-h4.q-pl-md.text-weight-bolder {{yahooPlayer.name.full}}
+            q-space
+            q-btn(icon="close" flat round dense @click="triggerDialog()")
 
-      q-card-section.row.items-center
-        .row.full-width
-          .col
-            q-table(
-              dense
-              flat
-              hide-bottom
-              :data="history"
-              :columns="columns"
-              row-key="_id"
-              :pagination.sync="pagination"
-            )
-              template(v-slot:body-cell-comment='props')
-                q-td(auto-width)
-                  | {{ props.row.comment }} #[span.text-grey-6 ({{ props.row.team.name }})]
+        q-card-section.row.items-center
+          .row.full-width
+            .col
+              q-table(
+                dense
+                flat
+                hide-bottom
+                :data="history"
+                :columns="columns"
+                row-key="_id"
+                :pagination.sync="pagination"
+              )
+                template(v-slot:body-cell-comment='props')
+                  q-td(auto-width)
+                    | {{ props.row.comment }} #[span.text-grey-6 ({{ props.row.team.name }})]
 
 </template>
 
