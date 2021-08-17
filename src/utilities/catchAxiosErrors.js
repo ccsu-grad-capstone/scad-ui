7106f4a74913c1e8f6c62f109501268331a5d682
Ryan Lauzon <lauzon232@gmail.com>
2021-08-17 10:16:18 -0400

    update login error message

--- src/utilities/catchAxiosErrors.js
+++ src/utilities/catchAxiosErrors.js
@@ -3,7 +3,11 @@ import notify from './nofity'
 const catchAxiosError = (error) => {
   console.error(error)
   if (error.response && error.response.data) {
-    notify.genericServerIssueWithResponse(error.response.data)
+    if (error.response.data.includes('html')) {
+      notify.error('Session has ended. Please login again.')
+    } else {
+      notify.genericServerIssueWithResponse(error.response.data)
+    }
   } else {
     notify.genericServerIssue()
   }
@@ -12,7 +16,11 @@ const catchAxiosError = (error) => {
 const catchAxiosNodeError = (error) => {
   console.error(error)
   if (error.response && error.response.data) {
-    notify.nodeServerIssueWithResponse(error.response.data)
+    if (error.response.data.includes('html')) {
+      notify.error('Session has ended. Please login again.')
+    } else {
+      notify.nodeServerIssueWithResponse(error.response.data)
+    }
   } else {
     notify.nodeServerIssue()
   }
