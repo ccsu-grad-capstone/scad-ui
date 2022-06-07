8394450c7b8f43711ba65fe934f661cdf6bc6b59
Ryan Lauzon <lauzon232@gmail.com>
2022-06-07 16:23:35 -0400

    updated emails to account for missing manager in settings.emails

--- src/components/dialogs/EditEmailsDialog.vue
+++ src/components/dialogs/EditEmailsDialog.vue
@@ -1,18 +1,21 @@
 <template lang="pug">
   q-dialog(v-model='visible' @hide="visible=false" persistent)
     q-card(style="width: 800px; max-width: 80vw;")
-      loading(v-if="!loaded" :message="'Fetching Player History...'")
+      loading(v-if="!loaded" :message="'Update League Emails...'")
       .div(v-else)
         q-card-section.row.items-center
           .row.full-width.text-h4.text-weight-bold League Emails
           .row.full-width.text-body1 Used to notify managers on illegal lineups, if desired.
         q-card-section.row.items-center
           q-list.full-width(separator dense)
-            q-item.row.full-width(v-for="(email, index) in emails" :key="index")
-              .column.col-6.justify-center
-                .text-weight-bold {{getTeam(email.guid).name}} #[span.text-caption ({{getTeam(email.guid).manager}})]
-              .col
-                q-input.q-my-xs(flat square filled dense v-model="email.email")
+            q-item(v-for="(team, index) in yahooTeams" :key="index")
+              .row.full-width
+                .column.col-6.justify-center
+                  .text-weight-bold {{team.name}} #[span.text-caption ({{getManager(team)}})]
+                .col
+                  .column(v-if="!checkManager(team)")
+                    q-btn.q-my-xs(square filled label="not found: add manager for team" size="md" color="primary" @click='addEmail(team)')
+                  q-input.q-my-xs(v-else flat square filled dense :value='getEmail(team)' @change="(e) => updateEmail(e, team)")
           .row.full-width.justify-center.q-pt-sm
             .col-5
               q-btn-group(spread)
@@ -36,7 +39,8 @@ export default {
     return {
       loaded: true,
       visible: true,
-      emails: []
+      emails: [],
+      email: ''
     }
   },
   mounted () {
@@ -54,18 +58,35 @@ export default {
     getTeamGuid () { return getTeamGuid }
   },
   methods: {
-    getTeam (guid) {
-      for (const team of this.yahooTeams) {
-        let teamGuid = this.getTeamGuid(team)
-        if (teamGuid === guid) return { name: team.name, manager: team.managers[0].nickname }
-      }
-    },
     async save () {
       this.loaded = false
       let scadSettings = JSON.parse(JSON.stringify(this.settings))
       scadSettings.emails = this.emails
       await this.$store.dispatch('league/saveLeagueSettings', { settings: scadSettings })
       this.$emit('close')
+    },
+    getManager (team) {
+      return team.managers[0].nickname
+    },
+    checkManager (team) {
+      return this.emails.find(e => e.guid === this.getTeamGuid(team)) || false
+    },
+    getEmail (team) {
+      return this.emails.find(e => e.guid === this.getTeamGuid(team)).email
+    },
+    getIndex (team) {
+      // if (this.getTeamGuid(team) === '2OMLCT3C2A42Z3FCGWJZCIDYLU') return -1
+      const i = this.emails.findIndex(e => {
+        return e.guid === this.getTeamGuid(team)
+      })
+      return i
+    },
+    updateEmail (e, team) {
+      this.emails[this.getIndex(team)].email = e.target.value
+    },
+    async addEmail (team) {
+      this.emails.push({ guid: getTeamGuid(team), email: '' })
+      await this.save()
     },
     cancel () {
       this.$emit('close')
